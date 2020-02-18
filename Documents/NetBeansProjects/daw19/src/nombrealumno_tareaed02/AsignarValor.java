f61d8185150f6d26997495a77a5bf6fdbdcfbec3
Garciber <Garciber@JGB-YOGA-710>
2020-02-18 19:35:43 +0100

    1ºVersión

---
+++ Documents/NetBeansProjects/daw19/src/nombrealumno_tareaed02/AsignarValor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombrealumno_tareaed02;
+
+/**
+ *
+ * @author Garciber
+ */
+public class AsignarValor {
+	public static void main(String[] args){
+	    int numero;
+	    numero = 10;
+	    System.out.println("El valor del número  es: " + numero);
+	}
+}
+
