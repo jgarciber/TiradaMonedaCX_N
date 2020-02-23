ad9b6fc6fb88b7b08be07c90458e6f9b876cbbef
Garciber <Garciber@JGB-YOGA-710>
2020-02-23 22:02:15 +0100

    Utiliza las funciones de refactorización para cambiar la cabecera del método imprimir. Le cambiarás el nombre por imprime. El método anterior tendrá que quedar en desuso. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio

--- Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
+++ Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
@@ -65,10 +65,10 @@ public class TiradaMonedaCX {
 				cont2 = 0;
 			}
 		}
-		imprimirResultado(contadorCaras, contadorCruces, maxCaraDobleConse, maxCruzDobleConse);
+		imprime(contadorCaras, contadorCruces, maxCaraDobleConse, maxCruzDobleConse);
 	}
 
-    public static void imprimirResultado(int contadorCaras, int contadorCruces, int maxCaraDobleConse, int maxCruzDobleConse) {
+    public static void imprime(int contadorCaras, int contadorCruces, int maxCaraDobleConse, int maxCruzDobleConse) {
         System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
         System.out.println("Maximo de caras dobles consecutivas: " + maxCaraDobleConse);
         System.out.println("Maximo de cruces dobles consecutivas: " + maxCruzDobleConse);
