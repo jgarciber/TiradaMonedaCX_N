5946d75f3ba85ce87525dc8d5068547f3e6d502e
Garciber <Garciber@JGB-YOGA-710>
2020-02-23 21:23:14 +0100

    Utiliza las funciones de refactorización de código, utilizando Eclipse y Netbeans, para que las variables y constantes de tu código tengan nombres significativos. Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
+++ Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class TiradaMonedaCX {
 	public static void main(String[] args) {
 		//las variables a y b servirán para guardar el resultado de la tirada de dos monedas
-		int a, b;
+		int resultado1, resultado2;
 		int contadorCaras = 0, contadorCruces = 0, numTiradas;
 		//las variables contador1 y contador2 servirán para guardar el número de caras y cruces dobles consecutivas
 		int cont1 = 0, cont2 = 0;
-		int maximoCarasDoblesConsecutivas = 0, maximoCrucesDoblesConsecutivas = 0;
+		int maxCaraDobleConse = 0, maxCruzDobleConse = 0;
 		boolean esCruzDoble = false, esCaraDoble = false;
 		String tirada1;
 		String tirada2;
@@ -18,17 +18,17 @@ public class TiradaMonedaCX {
 			numTiradas = teclado.nextInt();
 		} while (numTiradas < 1 || numTiradas > 50);
 		for (int i = 0; i < numTiradas; i++) {
-			a = (int) (Math.random() * 2);
-			b = (int) (Math.random() * 2);
+			resultado1 = (int) (Math.random() * 2);
+			resultado2 = (int) (Math.random() * 2);
 
-			if (a == 0) {
+			if (resultado1 == 0) {
 				contadorCaras++;
 				tirada1="cara";
 			} else {
 				contadorCruces++;
 				tirada1="cruz";
 			}
-			if (b == 0) {
+			if (resultado2 == 0) {
 				contadorCaras++;
 				tirada2 ="cara";
 			} else {
@@ -36,8 +36,8 @@ public class TiradaMonedaCX {
 				tirada2="cruz";
 			}
 			System.out.println("Moneda 1: " + tirada1 + ", Moneda 2: " + tirada2);
-			if (a == b) {
-				if (a == 0) {
+			if (resultado1 == resultado2) {
+				if (resultado1 == 0) {
 					esCaraDoble = true;
 					esCruzDoble = false;
 				} else {
@@ -50,23 +50,23 @@ public class TiradaMonedaCX {
 			}
 			if (esCaraDoble) {
 				cont1++;
-				if (cont1 > maximoCarasDoblesConsecutivas) {
-					maximoCarasDoblesConsecutivas = cont1;
+				if (cont1 > maxCaraDobleConse) {
+					maxCaraDobleConse = cont1;
 				}
 			} else {
 				cont1 = 0;
 			}
 			if (esCruzDoble) {
 				cont2++;
-				if (cont2 > maximoCrucesDoblesConsecutivas) {
-					maximoCrucesDoblesConsecutivas = cont2;
+				if (cont2 > maxCruzDobleConse) {
+					maxCruzDobleConse = cont2;
 				}
 			} else {
 				cont2 = 0;
 			}
 		}
 		System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
-		System.out.println("Maximo de caras dobles consecutivas: " + maximoCarasDoblesConsecutivas);
-		System.out.println("Maximo de cruces dobles consecutivas: " + maximoCrucesDoblesConsecutivas);
+		System.out.println("Maximo de caras dobles consecutivas: " + maxCaraDobleConse);
+		System.out.println("Maximo de cruces dobles consecutivas: " + maxCruzDobleConse);
 	}
 }
