ace131831ee6473ce35436209849853858090b9c
Garciber <Garciber@JGB-YOGA-710>
2020-03-02 20:50:29 +0100

    Documenta el código que has obtenido como resultado de todas las actividades anteriores de forma que quede claro lo que realiza: qué función realiza cada clase y/o método, cada uno de los parámetros necesarios, el autor, la versión, si se ha quedado algún método obsoleto y cualquier otra cuestión que consideres relevante. Lo realizarás tanto en Eclipse como en Netbeans. Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio

--- Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
+++ Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/TiradaMonedaCX.java
@@ -1,7 +1,14 @@
 package TiradaMoneda;
 import java.util.Scanner;
 public class TiradaMonedaCX {
-
+/**
+ * El método imprime se encarga de imprimir en pantalla los datos que se le pasan como parámetros.
+ * Estos datos son los resultados de los cálculos del método extraer.
+ * @param contadorCaras
+ * @param contadorCruces
+ * @param maxCaraDobleConse
+ * @param maxCruzDobleConse 
+ */
     public static void imprime(int contadorCaras, int contadorCruces, int maxCaraDobleConse, int maxCruzDobleConse) {
         System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
         System.out.println("Maximo de caras dobles consecutivas: " + maxCaraDobleConse);
--- Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/extraer.java
+++ Documents/NetBeansProjects/TiradaMonedaCX/src/TiradaMoneda/extraer.java
@@ -1,26 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TiradaMoneda;
 
 import java.util.Scanner;
 
 /**
- *
+ * 
  * @author Garciber
+ * @version 1.0
+ * Esta clase se encarga de contar el número de caras y cruces dobles consecutivas obtenidas al lanzar 
+ * dos moneda un número determinado de veces. El resultado de obtener cara o cruz en cada lanzamiento se realiza
+ * de genera aleatoriamente.
  */
 public class extraer {
 
     public static void main(String[] args) {
-        //las variables a y b servirán para guardar el resultado de la tirada de dos monedas
+        /**
+         *las variables resultado1 y resultado2 servirán para guardar el resultado (cara o cruz) de la tirada de dos monedas
+         *las variables contadorCaras y contadorCruces son acumuladores del número total de caras y cruzes simples obtenidas
+         * ya sean consecutivas o no consecutivas.
+         *las variables con1 y cont2 son acumuladores del número de caras o cruces dobles consecutivas obtenidas actualmente,
+         * se reinicia en el caso de perder la racha.
+         *las variables maxCaraDobleConse y maxCruzDobleConse almacenan el número máximo de caras y cruces consecutivas alcanzado(racha).
+        */
         int resultado1;
         int resultado2;
         int contadorCaras = 0;
         int contadorCruces = 0;
         int numTiradas;
-        //las variables contador1 y contador2 servirán para guardar el número de caras y cruces dobles consecutivas
         int cont1 = 0;
         int cont2 = 0;
         int maxCaraDobleConse = 0;
@@ -29,12 +34,21 @@ public class extraer {
         boolean esCaraDoble = false;
         String tirada1;
         String tirada2;
-        //hola
         Scanner teclado = new Scanner(System.in);
         do {
             System.out.print("Introduce el n\u00famero de veces que se van a lanzar las monedas (max 50): ");
             numTiradas = teclado.nextInt();
         } while (numTiradas < 1 || numTiradas > 50);
+        /**
+         * Se realiza la tirada de las dos monedas un número de veces numTiradas tal como introdujo el usuario,
+         * se obtiene un valor aleatorio resultante de calcular el producto de la función Math.random() que devuelve
+         * un valor entre [0,1) y el entero 2. Como al resultado del producto se le hace una conversión a entero, los únicos
+         * número enteros posibles en el rango [0,2) son el 0 y 1.
+         * El valor 0 se interpreta como cara.
+         * El valor 1 se interpreta como cruz.
+         * 
+         */
+        
         for (int i = 0; i < numTiradas; i++) {
             resultado1 = (int) (Math.random() * 2);
             resultado2 = (int) (Math.random() * 2);
@@ -53,6 +67,7 @@ public class extraer {
                 tirada2 = "cruz";
             }
             System.out.println("Moneda 1: " + tirada1 + ", Moneda 2: " + tirada2);
+            
             if (resultado1 == resultado2) {
                 if (resultado1 == 0) {
                     esCaraDoble = true;
@@ -82,6 +97,13 @@ public class extraer {
                 cont2 = 0;
             }
         }
+        /**
+         * Una vez calculdo:
+         *  el número de caras y cruces simples obtenidas
+         *  el número máximo de caras y cruces dobles obtenidas en alguna racha
+         * 
+         * Se le pasa al método imprime estos resultados para que se impriman por pantalla
+         */
         TiradaMonedaCX.imprime(contadorCaras, contadorCruces, maxCaraDobleConse, maxCruzDobleConse);
     }
     
